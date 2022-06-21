225015b465bc7af216c5bd8678c78e08b759c331
magersoft <magervlad@yandex.ru>
2022-06-21 09:39:45 +0200

    Add New Relic

--- main.ts
+++ main.ts
@@ -1,38 +1,18 @@
 require('dotenv').config();
 import * as http from 'http';
-import { Telegraf } from 'telegraf';
-import { RealCurrencyExchangeRate } from './src/RealCurrencyExchangeRate';
+import TelegramBot from './src/TelegramBot';
 
 const PORT = process.env.PORT || 3000;
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
-const bot = new Telegraf(BOT_TOKEN);
 
-bot.start((ctx) => {
-  let message = `Используйте команду /rate и укажите пару валют для которых вы хотите узнать реальный курс, например /rate RUBEUR`;
-  ctx.reply(message)
+const server = http.createServer(() => {
+  TelegramBot();
 });
 
-bot.command('rate', async (ctx) => {
-  try {
-    const { text } = ctx.update.message;
-    const currenciesSymbols = text.replace('/rate ', '');
-    const exchange = new RealCurrencyExchangeRate(currenciesSymbols);
-    const result = await exchange.get();
-
-    ctx.replyWithHTML(`
-По данным на ${result.time} MSK стоимость ${result.currency}:\n
-<strong>${result.rate} ${result.symbol}</strong>\n
-Доступные методы обмена: ${result.methods}\n
-Источник: <a href="https://p2p.binance.com/en/trade/all-payments/USDT?fiat=${result.fiat}">Binance</a>
-    `, { disable_web_page_preview: true })
-  } catch (error) {
-    ctx.reply('Укажите пару валют, например RUBEUR')
-  }
-});
-
-bot.launch();
-
-const server = http.createServer(() => {})
 server.listen(PORT, () => {
   console.log(`Real Currency Exchange Rate Bot was be started on port - ${PORT}`);
-})
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // For heroku. Stop idle application hack.
+  require('newrelic');
+}
---
+++ src/TelegramBot.ts
@@ -0,0 +1,33 @@
+import { Telegraf } from 'telegraf';
+import { RealCurrencyExchangeRate } from './RealCurrencyExchangeRate';
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+
+export default function TelegramBot() {
+  const bot = new Telegraf(BOT_TOKEN);
+
+  bot.start((ctx) => {
+    let message = `Используйте команду /rate и укажите пару валют для которых вы хотите узнать реальный курс, например /rate RUBEUR`;
+    ctx.reply(message)
+  });
+
+  bot.command('rate', async (ctx) => {
+    try {
+      const { text } = ctx.update.message;
+      const currenciesSymbols = text.replace('/rate ', '');
+      const exchange = new RealCurrencyExchangeRate(currenciesSymbols);
+      const result = await exchange.get();
+
+      ctx.replyWithHTML(`
+По данным на ${result.time} MSK стоимость ${result.currency}:\n
+<strong>${result.rate} ${result.symbol}</strong>\n
+Доступные методы обмена: ${result.methods}\n
+Источник: <a href="https://p2p.binance.com/en/trade/all-payments/USDT?fiat=${result.fiat}">Binance</a>
+    `, { disable_web_page_preview: true })
+    } catch (error) {
+      ctx.reply('Укажите пару валют, например RUBEUR')
+    }
+  });
+
+  bot.launch();
+}
